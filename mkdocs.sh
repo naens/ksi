4c960fc34bc5fb78cdd022b6303463b0ceef0072
naens <naens@gmx.com>
2018-12-15 21:15:19 +0100

    docs: update

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,1 +1,9 @@
-robodoc --src . --doc ./docs --html --multidoc --nodesc --sections --tell --toc --index --css robodoc.css
+robodoc --src . --doc ./docs --html --multidoc --nodesc \
+    --sections --tell --toc --index --css robodoc.css
+
+for f in docs/*.html
+do
+    sed -i -e 's/toc_index.html/index.html/' "$f"
+done
+
+mv docs/toc_index.html docs/index.html
