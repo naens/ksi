b7e22db8a305f04ffae582b059fcd82166350d3b
naens <naens@gmx.com>
2019-03-30 13:00:39 +0100

    update

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,9 +1,37 @@
-robodoc --src . --doc ./docs --html --multidoc --nodesc \
-    --sections --tell --toc --index --css robodoc.css
+#!/bin/sh
+function robodoc_html()
+{
+    robodoc --src . --doc ./docs --html --multidoc --nodesc \
+        --sections --tell --toc --index --css robodoc.css
 
-for f in docs/*.html
+    for f in docs/*.html
+    do
+        sed -i -e 's/toc_index.html/index.html/' "$f"
+    done
+
+    mv docs/toc_index.html docs/index.html
+}
+
+function robodoc_pdf()
+{
+    mkdir -p tex
+    robodoc --src . --doc tex/ksi --latex --singledoc --nodesc \
+        --sections --tell --toc --index
+    cd tex
+    pdflatex ksi.tex
+    if [ -f "ksi.pdf" ]
+    then
+        mv ksi.pdf ..
+    fi
+}
+
+for var in "$@"
 do
-    sed -i -e 's/toc_index.html/index.html/' "$f"
+    if [ "$var" = "html" ]
+    then
+        robodoc_html
+    elif [ "$var" = "pdf" ]
+    then
+        robodoc_pdf
+    fi
 done
-
-mv docs/toc_index.html docs/index.html
