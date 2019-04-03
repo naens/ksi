17a5201893ddf433619daa7c8c9db8bd2656b264
naens <naens@gmx.com>
2019-04-03 14:50:09 +0200

    add dos scripts

---
+++ dos-scripts/cpobj.bat
@@ -0,0 +1,12 @@
+@echo off
+:prompt
+set /p a=Copy .OBJ files here? Y/N?
+if /i "%a%" == "Y" goto copy
+if /i "%a%" == "N" goto exit
+goto :prompt
+
+:copy
+copy %KSI-DISK%\KSI\LIB\*.OBJ .
+
+:exit
+
